a820064080ec93f8b789b0df6f44ee84820b3ca8
Mike Solomon <mike@mikesolomon.org>
2018-12-08 19:50:36 +0200

    Adds short note

---
+++ example/index.ts
@@ -0,0 +1,13 @@
+// once you've generated the output file, you can use it like this
+
+// import {
+//     makeMakesAGroup,
+//     makeRegistersAUser,
+// } from "./generated/index";
+
+// makeRegistersAUser("myId", {
+//     firstName: "bar",
+//     hairColor: "black",
+//     id: "abc",
+//     lastName: "foo",
+// });
