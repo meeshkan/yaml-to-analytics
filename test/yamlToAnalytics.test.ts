e14af6cc29592e60b5e71f9f9a407f5e4a35c913
Kimmo Sääskilahti <ksaaskil@gmail.com>
2019-03-14 15:13:31 +0200

    Add tests.

--- test/yamlToAnalytics.test.ts
+++ test/yamlToAnalytics.test.ts
@@ -1,6 +1,7 @@
 import fs from "fs";
 import randomstring from "randomstring";
-import ymlFilesToTypeFile, { getRefs, resolveReferencePaths, substituteRefs } from "../src/yamlToAnalytics";
+import ymlFilesToTypeFile, {
+  buildFileContents, getRefs, resolveReferencePaths, substituteRefs } from "../src/yamlToAnalytics";
 
 test("resolveReferencePaths", () => {
   expect(resolveReferencePaths("/foo/bar", {
@@ -111,21 +112,30 @@ required:
 `,
 };
 
-test("ts json to make correct type file", async () => {
-  const parentDir = ".m33$hk4n_y4ml";
-  if (!fs.existsSync(`/tmp/${parentDir}`)) {
-    fs.mkdirSync(`/tmp/${parentDir}`);
+const PARENT_DIR = ".m33$hk4n_y4ml";
+
+const buildYamlAndTsDirs = () => {
+  if (!fs.existsSync(`/tmp/${PARENT_DIR}`)) {
+    fs.mkdirSync(`/tmp/${PARENT_DIR}`);
   }
   const dir = randomstring.generate();
-  fs.mkdirSync(`/tmp/${parentDir}/${dir}`);
-  const yamlDir = `/tmp/${parentDir}/${dir}/yaml`;
-  const tsDir = `/tmp/${parentDir}/${dir}/ts`;
+  fs.mkdirSync(`/tmp/${PARENT_DIR}/${dir}`);
+  const yamlDir = `/tmp/${PARENT_DIR}/${dir}/yaml`;
+  const tsDir = `/tmp/${PARENT_DIR}/${dir}/ts`;
   fs.mkdirSync(yamlDir);
   fs.mkdirSync(tsDir);
   for (const SCHEMA of Object.keys(SCHEMAS)) {
-    const kls = `Q${randomstring.generate()}`;
     const yamlFile = `${yamlDir}/${SCHEMA}.yml`;
     fs.writeFileSync(yamlFile, SCHEMAS[SCHEMA]);
   }
-  await ymlFilesToTypeFile(`${yamlDir}/**/*.yml`, `${tsDir}/index.ts`);
+  return [yamlDir, tsDir];
+};
+
+test("ts json to make correct type file", async () => {
+  const [yamlDir, tsDir] = buildYamlAndTsDirs();
+  const targetTs = `${tsDir}/index.ts`;
+  await ymlFilesToTypeFile(`${yamlDir}/**/*.yml`, targetTs);
+  const analyticsModule = await import(tsDir);
+  const registersAUserEvent = analyticsModule.makeRegistersAUser("foobar", {});
+  expect(registersAUserEvent.userId).toBe("foobar");
 });
